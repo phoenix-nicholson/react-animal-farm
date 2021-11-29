736c46d964ea0d469abb339329ac41c0ad663a2b
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-11-29 15:55:12 -0800

    Completed whole page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { animals } from '../../data'
+import './Animal.css'
 
-export default function Animal() {
-  return <></>
+export default function Animal(props) {
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+      <span className="name">{props.name}</span>
+      <span>{props.says}</span>
+    </div>
+  )
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import './Main.css'
+import background from '../../background.png'
+import { animals } from '../../data'
+import Animal from '../Animal/Animal'
 
 export default function Main() {
-  return <main></main>
+  return (
+    <main className="background" style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.name}
+          type={animal.type}
+          says={animal.says}
+          top={animal.top}
+          left={animal.left}
+        />
+      ))}
+    </main>
+  )
 }
