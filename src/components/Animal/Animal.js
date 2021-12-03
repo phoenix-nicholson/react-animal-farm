3f748150a4bc77f800a4757b4b1ae3f55dfba025
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-12-03 09:11:31 -0800

    destructured animal farm

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Animal.css'
 
-export default function Animal(props) {
+export default function Animal({ top, left, type, name, says }) {
   return (
-    <div className="animal" style={{ top: props.top, left: props.left }}>
-      <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
-      <span className="name">{props.name}</span>
-      <span>{props.says}</span>
+    <div className="animal" style={{ top: top, left: left }}>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <span className="name">{name}</span>
+      <span>{says}</span>
     </div>
   )
 }
